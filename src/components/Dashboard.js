2ff61da1a09fb45ddb0447dded6f75c991d2d95a
Mike Hagerty <mike.hagerty@heapanalytics.com>
2022-04-07 10:08:00 -0400

    Added upgrade radio selection on signup.html page.  Changed link on dashboard.js such that clicking upgrade leads to signup.html with Upgrade selected.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -337,8 +337,8 @@ const Dashboard = (props) => {
                         <div className="text-white font-medium text-5xl">Try new features</div>
                     </div>
                     <div className="mt-4 mr-auto md:mt-0 md:mr-0">
-                        <a href="https://www.primefaces.org/primeblocks-react" className="p-button font-bold px-5 py-3 p-button-warning p-button-rounded p-button-raised">
-                            Get Started
+                        <a href="/signup.html?plan=upgrade" className="p-button font-bold px-5 py-3 p-button-warning p-button-rounded p-button-raised">
+                            Upgrade! Get Started
                         </a>
                     </div>
                 </div>
